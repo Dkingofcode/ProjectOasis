f66543af913c95d4e673f77bcc2ece359a6d6634
David <doladepo128@gmail.com>
2024-10-17 20:57:06 -0700

    removed uploader and sales chart

--- src/features/dashboard/DashboardLayout.jsx
+++ src/features/dashboard/DashboardLayout.jsx
@@ -1,7 +1,7 @@
 import styled from 'styled-components';
 
 import DurationChart from './DurationChart';
-import SalesChart from './SalesChart';
+//import SalesChart from './SalesChart';
 import Stats from './Stats';
 import TodayActivity from '../check-in-out/TodayActivity';
 import { useRecentBookings } from './useRecentBooking';
@@ -40,7 +40,7 @@ function DashboardLayout() {
       />
       <TodayActivity />
       <DurationChart confirmedStays={confirmedStays} />
-      <SalesChart bookings={bookings} numDays={numDays} />
+      {/* <SalesChart bookings={bookings} numDays={numDays} /> */}
      
     </StyledDashboardLayout>
   );
